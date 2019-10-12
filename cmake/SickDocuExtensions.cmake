fb84ffb7840888096db086e1589d69a5077b824f
kaidoho <kho237115@gmail.com>
2019-10-12 19:55:26 +0200

    Initial commit

---
+++ cmake/SickDocuExtensions.cmake
@@ -0,0 +1,44 @@
+#######################################################################################################
+# Boilerplate for CMake
+#######################################################################################################
+
+
+#######################################################################################################
+# GenerateDocumentation()
+#######################################################################################################
+function(GenerateDocumentation)
+
+  find_package(Doxygen)
+  if (DOXYGEN_FOUND)
+    set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/doxygen/Doxyfile.in)
+    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+    configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+    add_custom_target( RunDoxygen
+      COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      VERBATIM )
+  else ()
+    message(STATUS "GenerateDocumentation: Doxygen was not found")
+    message(STATUS "GenerateDocumentation: Not creating target GenerateDoxygenDocumentation")
+  endif ()
+
+
+  find_package(Sphinx)
+  if (SPHINX_FOUND)
+    set(SPHINX_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/sphinx)
+    set(SPHINX_BUILD ${CMAKE_CURRENT_BINARY_DIR}/sphinx)
+
+    add_custom_target(RunSphinx ALL
+                      COMMAND
+                      ${SPHINX_EXECUTABLE} -b html
+                      -Dbreathe_projects.SSBL=${SPHINX_BUILD}/../doxygen/xml
+                      ${SPHINX_SOURCE} ${SPHINX_BUILD}
+                      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                      DEPENDS  RunDoxygen)
+  else ()
+    message(STATUS "GenerateDocumentation: Sphinx was not found")
+    message(STATUS "GenerateDocumentation: Not creating target Sphinx")
+  endif ()
+endfunction()
+
+
---
+++ src/docu/sphinx/index.rst
@@ -0,0 +1,27 @@
+.. SSBL documentation master file, created by
+   sphinx-quickstart on Tue Oct  8 22:46:36 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SSBL's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+ 
+Docs
+====
+ 
+.. doxygenclass:: ssbl::SickLidar2d
+   :members: 
